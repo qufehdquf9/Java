6b9b839ce0f1528d9fc09c85e251056912f8f592
choi youngjin <cyjin9.yc@gmail.com>
2018-04-09 01:53:55 +0900

    gugudan

--- src/main.java
+++ src/main.java
@@ -1,5 +1,14 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("--------구구--------");
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println("--------"+i+"단--------");
+            for(int j = 1 ; j<10;j++){
+
+                System.out.println(i+"*"+j+"="+i*j);
+            }
+        }
+
     }
 }
